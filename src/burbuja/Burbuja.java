54a31a573a6a7460a5bdd7ea8001b5562f5672d1
HazaelJDev <jejibarrera@gmail.com>
2017-12-10 00:47:32 -0600

    Metodos de ordenamiento burbuja acabados

--- src/burbuja/Burbuja.java
+++ src/burbuja/Burbuja.java
@@ -1,4 +1,34 @@
 package burbuja;
-public class Burbuja {
-    
+public class Burbuja{
+    long i=0,j=0;
+    int aux=0; //variable auxiliar para guardar el numero menor por un momento
+    Nodo na=null,ns=null; //na: Nodo actual; ns = Nodo siguiente;
+    //Método de ordenamiento burbuja para arreglos
+    public void sortArray(int a[]){
+        for (i=1;i<a.length;i++){
+            for(j=0;j<(a.length-i);j++){
+                if(a[j]>a[j+1]){
+                    aux = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = aux;
+                }
+            }
+        }
+    }
+    //Método de ordenamiento burbuja para listas simplemente enlazadas
+    public void sortListSE(ListaSE list){
+        for(i=1;i<list.length;i++){
+            na = list.primero;
+            ns = na.sig;
+            for(j=0;j<(list.length-i);j++){
+                if(na.dat>ns.dat){
+                    aux = na.dat;
+                    na.dat = ns.dat;
+                    ns.dat = aux;
+                }
+                na = na.sig;
+                ns = ns.sig;
+            }
+        }
+    }
 }
