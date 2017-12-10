ce9b7fccc221524cf0eb567f5e00ebddeb9a71a5
HazaelJDev <jejibarrera@gmail.com>
2017-12-10 02:02:47 -0600

    Test 2, exitoso

--- src/burbuja/TestBurbuja.java
+++ src/burbuja/TestBurbuja.java
@@ -1,11 +1,16 @@
 package burbuja;
+import java.util.Random;
 public class TestBurbuja {
     public static void main(String args[]){
         Burbuja burbuja = new Burbuja();
         int arreglo[] = {80,39,7,1,25,3,1839,2,45};
+        Random r = new Random();
         ListaSE lista =  new ListaSE();
-        for (int var : arreglo) {
+        /*for (int var : arreglo) {
             lista.addNodo(var);
+        }*/
+        for(int i=1; i<=500; i++){
+            lista.addNodo(r.nextInt(4500));
         }
         System.out.println("\tArreglo");
         for (int var : arreglo) {
