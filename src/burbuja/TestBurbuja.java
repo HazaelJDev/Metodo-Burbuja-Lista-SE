0c9db30a295ce674d8b0ea7838f06f3ecfb31b2f
HazaelJDev <jejibarrera@gmail.com>
2017-12-13 03:18:40 -0600

    Lectura del CSV, guardado de los datos en las 3 listas y ordenamiento de dichas listas

--- src/burbuja/TestBurbuja.java
+++ src/burbuja/TestBurbuja.java
@@ -1,30 +1,38 @@
 package burbuja;
-import java.util.Random;
+import com.csvreader.CsvReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class TestBurbuja {
     public static void main(String args[]){
+        //Instanciamos la clase Burbuja para poder usar el método de ordenamiento en las listas dinamicas
         Burbuja burbuja = new Burbuja();
-        int arreglo[] = {80,39,7,1,25,3,1839,2,45};
-        Random r = new Random();
-        ListaSE lista =  new ListaSE();
-        /*for (int var : arreglo) {
-            lista.addNodo(var);
-        }*/
-        for(int i=1; i<=500; i++){
-            lista.addNodo(r.nextInt(4500));
+        //Creamos 3 listas dinamicas cada una guardara los mismos datos, sólo que estaran diferentemente desacomodamos
+        ListaSE lpoco = new ListaSE();
+        ListaSE lmedio = new ListaSE();
+        ListaSE lmuy = new ListaSE();
+        /*
+            Leemos el CSV y guardamos los 100,000 datos en cada lista.
+            Hay 3 columnas cada una tiene los mismos datos, sólo que desordenados diferentemente.
+        */
+        try {
+            CsvReader datos = new CsvReader("Datos.csv");
+            datos.readHeaders();
+            while (datos.readRecord()) {
+                lpoco.addNodo(Integer.parseInt(datos.get("Poco_Desordenados")));
+                lmedio.addNodo(Integer.parseInt(datos.get("Medio_Desordenados")));
+                lmuy.addNodo(Integer.parseInt(datos.get("Muy_Desordenados")));
+            }
+            datos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("\tArreglo");
-        for (int var : arreglo) {
-            System.out.print("|"+var+"|");
-        }
-        System.out.println("\n\n\tArreglo Ordenado");
-        burbuja.sortArray(arreglo);
-        for (int var : arreglo) {
-            System.out.print("|"+var+"|");
-        }
-        System.out.println("\n\n\tLista");
-        lista.showList();
-        System.out.println("\n\tLista Ordenada");
-        burbuja.sortListSE(lista);
-        lista.showList();
+        
+        //Ordenamos listas
+        burbuja.sortListSE(lpoco);
+        burbuja.sortListSE(lmedio);
+        burbuja.sortListSE(lmuy);
     }
 }
